86041655cd8387270ed49c85422eb76b2f5e3e33
Sergey <serg_2004@inbox.ru>
2021-08-16 12:10:52 +0700

    soap add/delete project fixed

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config["web"]["baseUrl"] + "api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
             return False
 
     def get_projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config["web"]["baseUrl"] + "api/soap/mantisconnect.php?wsdl")
         projects_list = []
         for project in client.service.mc_projects_get_user_accessible(username, password):
             projects_list.append(Project(name=project.name, id=project.id))
